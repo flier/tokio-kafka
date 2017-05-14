bfb24056b8b8890edfd0c60c5f15c75c9d37b6c3
Flier Lu <flier.lu@gmail.com>
2017-05-14 17:49:16 +0800

    rename dns mod to resolver

--- src/network/mod.rs
+++ src/network/mod.rs
@@ -1,7 +1,7 @@
 mod request;
 mod response;
 mod codec;
-mod dns;
+mod resolver;
 mod stream;
 mod conn;
 mod pool;
@@ -9,7 +9,7 @@ mod pool;
 pub use self::request::{KafkaRequest, TopicPartition};
 pub use self::response::KafkaResponse;
 pub use self::codec::KafkaCodec;
-pub use self::dns::{DnsQuery, DnsResolver, Resolver};
+pub use self::resolver::{DnsQuery, DnsResolver, Resolver};
 pub use self::stream::{Connect, KafkaConnector, KafkaStream};
 pub use self::conn::{KafkaConnection, KeepAlive, Status};
 pub use self::pool::{Pool, Pooled};
